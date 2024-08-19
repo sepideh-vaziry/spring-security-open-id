98b55e91ad89f318180482ca3f33581d7a0071a2
Sepideh Vaziry <sepideh.vaziry@gmail.com>
2024-08-19 12:23:11 +0330

    First commit: Create Spring Boot project

---
+++ src/main/java/com/example/springsecurityopenid/SpringSecurityOpenIdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityopenid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOpenIdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOpenIdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurityopenid/SpringSecurityOpenIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityopenid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOpenIdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
